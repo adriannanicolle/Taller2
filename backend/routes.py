80be236c2aaf3a2a42951d385b44644684086b98
samedina1234 <samedina@tes.edu.ec>
2024-10-21 22:43:49 -0500

    conexion del frontend con el backned

--- backend/routes.py
+++ backend/routes.py
@@ -18,7 +18,7 @@ def create_friend():
         #Validaciones :D
         required_fields = ["name", "role", "description", "gender"]
         for field in required_fields:
-            if field not in data:
+            if field not in data or not data.get(field):
                 return jsonify({"error":f'Faltan campos obligatorios: {field}'}),400
 
         name = data.get("name")
@@ -39,7 +39,7 @@ def create_friend():
         db.session.add(new_friend)
         db.session.commit()
 
-        return jsonify({"msg":"Amigo creado exitosamente"}),201
+        return jsonify(new_friend.to_json()),201
 
     except Exception as e:
         db.session.rollback()
